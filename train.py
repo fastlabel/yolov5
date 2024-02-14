a5dae0f5366bb597455037d74a8ef579a5058b98
tsuba-neko <tsubasa.kaneko@fastlabel.ai>
2024-02-14 19:15:22 +0900

    add checkpoint

--- train.py
+++ train.py
@@ -384,6 +384,7 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
                 if best_fitness == fi:
                     torch.save(ckpt, best)
                 if opt.save_period > 0 and epoch % opt.save_period == 0:
+                    LOGGER.info(f'Saving checkpoint at {epoch} epochs')
                     torch.save(ckpt, w / f'epoch{epoch}.pt')
                 del ckpt
                 callbacks.run('on_model_save', last, epoch, final_epoch, best_fitness, fi)
